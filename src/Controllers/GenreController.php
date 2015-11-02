5f7e8ef7b14535f28e288053ba91c35d2e78f3cf
FreeFly19 <you@example.com>
2015-11-03 01:05:27 +0200

    GenreController completed

--- src/Controllers/GenreController.php
+++ src/Controllers/GenreController.php
@@ -8,8 +8,32 @@
 
 namespace Controllers;
 
+use Models\Genre;
 
 class GenreController  extends BaseController
 {
+    public function Index()
+    {
+        return json_encode(Genre::find());
+    }
+
+    public function Add()
+    {
+        $genre = new Genre();
+        $genre->name = $this->getData()->name;
+        $genre->save();
+    }
+
+    public function Delete()
+    {
+        $id = $this->getData()->id;
+        Genre::findById($id)->remove();
+    }
+
+    public function GetBooks()
+    {
+        $id = $this->getData()->id;
+        return json_encode(Genre::findById($id)->getBooks());
+    }
 
 } 
