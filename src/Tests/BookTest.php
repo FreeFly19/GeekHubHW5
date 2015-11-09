31b044efc1eb6e1da0499d73a9d145365cd01db8
FreeFly19 <you@example.com>
2015-11-10 00:49:39 +0200

    BookTest success

--- src/Tests/BookTest.php
+++ src/Tests/BookTest.php
@@ -8,11 +8,62 @@
 
 namespace Tests;
 
+use Models\Book;
 
 class BookTest extends \PHPUnit_Framework_TestCase{
 
-
-    public function testFirst(){
-        $this->assertTrue(true);
+    public function getPdo(){
+        return $this->getMockBuilder("\\PDO")
+            ->disableOriginalConstructor()
+            ->getMock();
     }
+
+    public function testGetTableName(){
+        $this->assertEquals("books", Book::getTableName());
+    }
+
+    public function testParent(){
+        Book::setPDO($this->getPdo());
+        $this->assertInstanceOf("App\\AbstractModel", new Book());
+    }
+
+    public function testInsert(){
+        $pdo = $this->getPdo();
+        $pdo->method("lastInsertId")->willReturn(1234);
+
+        $stmt = $this->getMock("\\PDOStatement", ['bindParam']);
+
+        $pdo->method("prepare")->willReturn($stmt);
+
+        Book::setPDO($pdo);
+
+        $book = new Book();
+        $this->assertNull($book->id);
+
+        $book->save();
+        $this->assertEquals(1234, $book->id);
+    }
+
+    public function testPdoSetterGetter(){
+        $pdo = $this->getPdo();
+        Book::setPDO($pdo);
+
+        $this->assertEquals($pdo, Book::getPDO());
+    }
+
+    public function testRemove(){
+        $pdo = $this->getPdo();
+        $stmt = $this->getMock("\\PDOStatement", ['bindParam']);
+        $pdo->method("prepare")->willReturn($stmt);
+
+        Book::setPDO($pdo);
+
+        $book = new Book();
+        $book->id = 15;
+        $book->remove();
+
+        $this->assertNull($book->id);
+    }
+
+
 } 
