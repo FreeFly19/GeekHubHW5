e3fe9f0decd555404ae36be43ba6a68a601f30cf
FreeFly19 <you@example.com>
2015-11-09 23:29:51 +0200

    First test created

---
+++ src/Tests/BookTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FreeFly
+ * Date: 09.11.15
+ * Time: 23:26
+ */
+
+namespace Tests;
+
+
+class BookTest extends \PHPUnit_Framework_TestCase{
+
+
+    public function testFirst(){
+        $this->assertTrue(true);
+    }
+} 
